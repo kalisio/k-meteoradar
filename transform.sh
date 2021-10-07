03318e4dfa915ad805cf0f0c0b0849725e7ab7b9
cnouguier <christophe.nouguier@free.fr>
2021-10-07 22:12:59 +0200

    Revert to GeoTiff instead of COG du to merge issue

--- transform.sh
+++ transform.sh
@@ -21,7 +21,7 @@ gdal_translate \
   -a_srs "+title=thomas +proj=stere +lat_0=90 +lat_ts=45 +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs" \
   -a_ullr -619652.074 -3526818.338 916347.926 -5062818.338 \
   -a_nodata 0 \
-  -of COG \
+  -of GTiff -co TILED=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 -co COPY_SRC_OVERVIEWS=YES \
   ${PNG_FILE} ${TIF_FILE}.tmp
 
 if [ $? -ne 0 ]; then
@@ -34,7 +34,7 @@ fi
 gdalwarp \
   -overwrite \
   -t_srs EPSG:4326 \
-  -of COG \
+  -of GTiff -co TILED=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 -co COPY_SRC_OVERVIEWS=YES \
   -co COMPRESS=DEFLATE \
   ${TIF_FILE}.tmp ${TIF_FILE}
 
