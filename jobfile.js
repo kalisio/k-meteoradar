d61f659745d1c5832b783bcc25a478e59814744e
cnouguier <christophe.nouguier@free.fr>
2021-10-19 16:45:25 +0200

    wip: removed unnecessary S3_REGION

--- jobfile.js
+++ jobfile.js
@@ -101,7 +101,6 @@ module.exports = {
           options: {
             client: {
               endpoint: process.env.S3_ENDPOINT,
-              region: process.env.S3_REGION,
               accessKeyId: process.env.S3_ACCESS_KEY,
               secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
             },
